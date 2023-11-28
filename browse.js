5abb45712d3012f33bc78d49ebb5f47b47547ad3
Ginelle Temull <89558840+ginellego@users.noreply.github.com>
2023-11-28 03:28:58 +0000

    styling and on click services

---
+++ browse.js
@@ -0,0 +1,12 @@
+var cHeader = "Contractor List"; 
+
+function initializeBrowse(){
+
+}
+
+
+
+function updateCHeader (service){
+    document.getElementById('cHeader').innerHTML = cHeader + ": " + service;
+    
+}
