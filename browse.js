4505ec12f175a5425be0439f70013826429d4b3e
Ginelle Temull <89558840+ginellego@users.noreply.github.com>
2023-11-30 16:50:20 +0000

    sort work

--- browse.js
+++ browse.js
@@ -112,126 +112,62 @@ function w3RemoveClass(element, name) {
 // radius
 // distance
 
+function sortSelection(c) {
+  if(c == "priceAsc"){
+    w3RemoveClass()
+  }else if(){
+
+  }else if(){
+
+  }else if(){
+
+  }else if(){
+
+  }else if(){
+
+  }
+  var x, i;
+  x = document.getElementsByClassName("sortAll");
+  if (c == "sortAll") c = "";
+  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+  for (i = 0; i < x.length; i++) {
+    w3RemoveClass(x[i], "show");
+    w3RemoveClass(x[i], "hide")
+    if (x[i].className.indexOf(c) > -1){
+      w3AddClass(x[i], "show");
+    } else {w3AddClass(x[i], "hide"); }
+  }
+}
+
+
+
+
 
 function filterSort(type, value){
-  if(type == "reviews"){
-      sortByRating(value); 
-  }else if(type == "price"){
+  if(type == "price"){
       sortByPrice(value); 
-  }else if(type == "distance")
-      sortByDistance(value); 
-}
-function sortByRating(value){
-  if(value == "top-rated"){
-
-  }else if(value == "highest-rating"){
-
-  }else if(value == "low-rating"){
-
+  }else{
+    sortSelection(value); 
   }
-}
-
-function topRated(){
-  active = document.getElementsByClassName("show");
-
-}
-
-function highestRating(){
-  active = document.getElementsByClassName("show");
-}
-
-function lowRating(){
-  active = document.getElementsByClassName("show");
-}
-
-function sortByDistance(value){
-  if(value == "distance"){
-
-  }else if(value == "radius"){
-
-  }else if(value == "postal"){
+  
       
-  }
 }
 
-function radiusDistance(){
-  active = document.getElementsByClassName("show");
-}
 
-function postal(){
-  active = document.getElementsByClassName("show");
-
-}
 
 function sortByPrice(value){
   if(value == "high-low"){
-    priceOrder("desc");
+    
   }else if (value == "low-high"){
-    priceOrder("asc");
-    // SortData(); 
-    // sort(); 
+     
   }else{
     filterSelection(value); 
   }
   
 }
 
-function priceOrder(direction){
-  var i, list, low, mid, high,parent; 
-  parent = document.getElementById('clist'); 
-  parent.innerHTML = ""; 
-  list = document.getElementsByClassName("all"); 
-  list= Array.prototype.slice.call(list, 0);
-  low= list.getElementsById("$1"); 
-  low== Array.prototype.slice.call(low, 0);
-  mid= list.getElementsById("$2"); 
-  mid= Array.prototype.slice.call(mid, 0);
-  high = list.getElementsById("$3"); 
-  high== Array.prototype.slice.call(high, 0);
-
-  if(direction == "asc"){
 
 
-
-
-
-    // for(i = 0, i<low.length; i++;){
-    //   list[i] = low[i]; 
-    // }
-    // for(i, i<low.length + mid.length-1; i++;){
-    //   list[i] = mid[i-low.length]; 
-    // }
-    // for(i, i<low.length+mid.length+high.length; i++;){
-    //   list[i] = high[i-low.length-mid.length]; 
-    // }
-    // sort(); 
-  }else if(direction == "desc"){
-
-
-
-
-
-  //   for(i = 0, i<high.length; i++;){
-  //     list[i] = high[i]; 
-  //   }
-  //   for(i, i<high.length + mid.length-1; i++;){
-  //     list[i] = mid[i-high.length]; 
-  //   }
-  //   for(i, i<low.length+mid.length+high.length; i++;){
-  //     list[i] = low[i-high.length-mid.length]; 
-  //   }
-  // }
-
-  // for(var i = 0, l = list.length; i < l; i++) {
-  //   parent.appendChild(list[i]);
-}
-}
-
-
-
-function priceLowHigh(){
-
-}
 
 
 
