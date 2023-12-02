7398afd3da0b055b4e39f67159cff3ea1e5d8fbb
toranp <150299707+toranp@users.noreply.github.com>
2023-12-01 21:31:08 -0600

    Add files via upload

--- quote.js
+++ quote.js
@@ -1,12 +1,34 @@
 //function that works onclick for get a quote
 function getAQuote() {
-    alert("Request for a quote has been submitted successfully!");
-    goToUserHomePage();
+    if(validateForm()) {
+      alert("Request for a quote has been submitted successfully!");
+      goToUserHomePage();
+    }
   }
   
   // ------------------- JS FOR LINKING TO USER HOME PAGE ----------------------------
   function goToUserHomePage() {
-    var targetPage = "user_home.html";
-    window.location.href = targetPage;
+    // if(validateForm()) {
+      var targetPage = "user_home.html";
+      window.location.href = targetPage;
+    // }
   }
   
+  function validateForm() {
+    let name = document.forms["quote-form"]["name"].value;
+    let email = document.forms["quote-form"]["email"].value;
+    let phone = document.forms["quote-form"]["phone"].value;
+    if (name == "") {
+      alert("Name must be filled out");
+      return false;
+    }
+    if( email == "") {
+      alert("Email must be filled out");
+      return false;
+    }
+    if( phone == "") {
+      alert("Phone number must be filled out");
+      return false;
+    }
+    return true;
+  }
